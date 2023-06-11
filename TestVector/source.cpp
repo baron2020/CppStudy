ff918a357820892467d24bfaef2607d6c2aeddd7
k.yuka <61102173+baron2020@users.noreply.github.com>
2023-06-11 17:46:52 +0900

    Add files via upload

---
+++ TestVector/source.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+//動的配列クラス std::vector の使い方
+
+int main(void) {
+    std::vector<int> v;
+    v = { 1, 2, 3, 4, 5 };
+
+    for (int i = 0; i < v.size(); i++)
+        std::cout << v[i] << "\n";
+    std::cout << "終了しました。\n";
+    
+    return 0;
+}
