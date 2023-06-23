6ff5b76d17d0fbbf51532c3fe771fc0bd035f431
k.yuka <61102173+baron2020@users.noreply.github.com>
2023-06-23 21:31:40 +0900

    Add files via upload

--- TestReadText/source2.cpp
+++ TestReadText/source2.cpp
@@ -1,16 +1,49 @@
-#include <vector>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
-//動的配列クラス
-//std::vectorの使い方
+//test1.txtの中身を読み込み、カンマで区切り、vectorに格納する。
+//ベクターの全要素を表示をする。
 
-int main(void) {
-    std::vector<int> v;
-    v = { 1, 2, 3, 4, 5 };
 
-    for (int i = 0; i < v.size(); i++)
-        std::cout << v[i] << "\n";
-    std::cout << "終了しました。\n";
+std::vector<std::string> split(std::string str) {
+    std::string separator = ",";
+    std::vector<std::string> result;
+    std::string tstr = str + separator;
+    long l = tstr.length(), sl = separator.length();
+    std::string::size_type pos = 0, prev = 0;
 
-    return 0;
+    for (; pos < l && (pos = tstr.find(separator, pos)) != std::string::npos; prev = (pos += sl)) {
+        result.emplace_back(tstr, prev, pos - prev);
+    }
+    std::cout << "resultサイズ：" << result.size() << '\n';
+    return result;
+}
+
+int main(){
+    std::vector<std::vector<std::string>> all_v;
+
+    std::ifstream file("test1.txt");  // 読み込むファイルのパスを指定
+    std::string line;
+
+    std::cout << "出力開始" << std::endl;
+    //最終点に達するまで
+    while (file) { 
+        file >> line; //fileからlineにデータを格納
+        std::cout << line << std::endl; //lineを表示
+        std::vector<std::string> temp_v = split(line);
+        //all_vの最後に追加
+        all_v.push_back(temp_v);
+    }
+    std::cout << "出力終了" << std::endl;
+    std::cout << "ベクターサイズ：" <<all_v.size() << std::endl;
+
+    //size_t：そのオブジェクトのサイズを表現するのに十分な精度を持つことが保証されている。
+    for (size_t i = 0; i < all_v.size(); ++i) {
+        for (size_t j = 0; j < all_v.size(); ++j) {
+            std::cout << "[i]:" << i << " [j]:" << j << " :" << all_v[i][j] << std::endl;
+        }
+    }
+    return (0);
 }
