db947a837c4f14324444739b90f99f31855fd29a
k.yuka <61102173+baron2020@users.noreply.github.com>
2023-06-24 21:51:45 +0900

    Add files via upload

--- TestReadText/source1.cpp
+++ TestReadText/source1.cpp
@@ -2,20 +2,26 @@
 #include <iostream>
 #include <string>
 
-//読み込み対象のテキストをエンコードUTF-8に変更する（UTF-8(BOM付き)だと余計な文字化けが追加されたため）
-//test1.txt：読み込み対象のファイル。sorce1ファイルと同じディレクトリに格納した。
+//読み込み対象のテキストをエンコードUTF-8に変更する。（UTF-8(BOM付き)だと余計な文字化けが追加されたため）
+//test1.txtは、読み込み対象のファイル。sorce1ファイルと同じディレクトリに格納した。
+//最初のif文で、読み込み対象ファイルが存在するか確認をしている。
 
-int main(){
+int main() {
     std::ifstream file("test1.txt");  // 読み込むファイルのパスを指定
     std::string line;
-
-    std::cout << "出力開始" << std::endl;
-    //最終点に達するまで
-    while (file) { 
-        file >> line; //fileからlineにデータを格納
-        std::cout << line << std::endl; //lineを表示
+    
+    if (!file) {
+        //エラー処理
+        std::cout << "ファイルが存在しません。" << std::endl;
     }
-    std::cout << "出力終了" << std::endl;
+    else {
+        std::cout << "出力開始" << std::endl;
+        //1行ずつ読み込む
+        while (std::getline(file, line)) {
+            std::cout << line << std::endl;
+        }
+        std::cout << "出力終了" << std::endl;
+    }
 
     return (0);
 }
